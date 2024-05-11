d9ac0747287d5a89b4462b1f143fcfc23af56be6
Kaveen Dinethma <kaveen.dinethma@gmail.com>
2024-05-11 23:44:40 +0530

    TP | Onboarding two and other basic updates

--- app/src/main/java/com/example/taskpivot/modules/OnboardingScreen02.kt
+++ app/src/main/java/com/example/taskpivot/modules/OnboardingScreen02.kt
@@ -1,14 +1,33 @@
 package com.example.taskpivot.modules
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import com.example.taskpivot.R
+import com.example.taskpivot.databinding.ActivityOnboardingScreen01Binding
+import com.example.taskpivot.databinding.ActivityOnboardingScreen02Binding
 
 class OnboardingScreen02 : AppCompatActivity() {
+
+    private lateinit var binding : ActivityOnboardingScreen02Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_onboarding_screen02)
         enableEdgeToEdge()
+
+        binding = ActivityOnboardingScreen02Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.skipText01.setOnClickListener{
+            val intent2 = Intent( this, HomeScreen::class.java)
+            startActivity(intent2)
+            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+        }
+
+        binding.onboardingbtn01.setOnClickListener {
+            val intent1 = Intent(this, HomeScreen::class.java)
+            startActivity(intent1)
+        }
+
     }
 }
