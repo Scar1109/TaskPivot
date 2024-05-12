1bba272a199cd0c9117c2dec84dd99473a482f71
Kaveen Dinethma <kaveen.dinethma@gmail.com>
2024-05-12 08:06:54 +0530

    TP | Greeting section and other basic updates

--- app/src/main/java/com/example/taskpivot/modules/HomeScreen.kt
+++ app/src/main/java/com/example/taskpivot/modules/HomeScreen.kt
@@ -7,6 +7,9 @@ import androidx.activity.enableEdgeToEdge
 import com.example.taskpivot.R
 import com.example.taskpivot.databinding.ActivityHomeScreenBinding
 import com.example.taskpivot.databinding.ActivityOnboardingScreen01Binding
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 class HomeScreen : AppCompatActivity() {
 
@@ -19,6 +22,7 @@ class HomeScreen : AppCompatActivity() {
         binding = ActivityHomeScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+//        Bottom Navigation bar
         binding.bottomNavigation.selectedItemId = R.id.page_1
 
         binding.bottomNavigation.setOnNavigationItemSelectedListener { item ->
@@ -40,6 +44,32 @@ class HomeScreen : AppCompatActivity() {
                 }
                 else -> false
             } }
+
+//        Greeting message and image
+
+        val currentTime = Calendar.getInstance()
+        val currentHour = currentTime.get(Calendar.HOUR_OF_DAY)
+
+        val greetingMessage = when (currentHour) {
+            in 6..11 -> "Good morning"
+            in 12..16 -> "Good afternoon"
+            else -> "Good evening"
+        }
+        binding.greeting.text = greetingMessage
+
+        // Format date
+        val dateFormat = SimpleDateFormat("EEEE, MMMM dd, yyyy", Locale.getDefault())
+        val formattedDate = dateFormat.format(currentTime.time)
+        binding.date.text = formattedDate
+
+        val imageResId = when {
+            currentHour in 4..9 -> R.drawable.morining_sun
+            currentHour in 10..15 -> R.drawable.noon_sun
+            currentHour in 16 .. 17 -> R.drawable.evenining_sun
+            else -> R.drawable.night_moon
+        }
+
+        binding.timeImg.setImageResource(imageResId)
 
     }
 }
