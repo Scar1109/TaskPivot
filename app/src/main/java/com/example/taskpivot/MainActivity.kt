1940abc6c2c62db9a4e17d473fb1674ad0f51a82
Kaveen Dinethma <kaveen.dinethma@gmail.com>
2024-05-14 17:18:55 +0530

    TP | Onboarding screen bug fix

--- app/src/main/java/com/example/taskpivot/MainActivity.kt
+++ app/src/main/java/com/example/taskpivot/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.taskpivot
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import androidx.activity.enableEdgeToEdge
+import com.example.taskpivot.modules.HomeScreen
 import com.example.taskpivot.modules.OnboardingScreen01
 
 class MainActivity : AppCompatActivity() {
@@ -17,9 +19,29 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         handler.postDelayed({
-            startActivity(Intent(this, OnboardingScreen01::class.java))
+            if (isUserLoggedInOnce()) {
+                // User has logged in before, navigate to HomeScreen
+                startActivity(Intent(this, HomeScreen::class.java))
+            } else {
+                // User has not logged in before, show OnboardingScreen
+                setUserLoggedInOnce()
+                startActivity(Intent(this, OnboardingScreen01::class.java))
+
+            }
             finish()
             overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
         }, 2500L)
+    }
+
+    private fun isUserLoggedInOnce(): Boolean {
+        val sharedPreferences = getSharedPreferences("taskPivotPrefs", Context.MODE_PRIVATE)
+        return sharedPreferences.getBoolean("userLoggedInOnce", false)
+    }
+
+    private fun setUserLoggedInOnce() {
+        val sharedPreferences = getSharedPreferences("taskPivotPrefs", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putBoolean("userLoggedInOnce", true)
+        editor.apply()
     }
 }
